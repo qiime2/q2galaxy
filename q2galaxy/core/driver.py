0b19f78ba2e314c897892863666d8d48cfe3281b
Anthony <oddant1@hotmail.com>
2020-05-14 14:02:19 -0700

    SQUASH: Some extra param support

--- q2galaxy/core/driver.py
+++ q2galaxy/core/driver.py
@@ -1,3 +1,5 @@
+import os
+
 import qiime2.sdk as sdk
 
 
@@ -6,10 +8,22 @@ def action_runner(plugin_id, action_id, inputs):
     plugin = pm.get_plugin(id=plugin_id)
     action = plugin.actions[action_id]
 
+    # This is sloppy and should probably be handled higher up by passing inputs
+    # and params to this function as seperate entities in the first place
+    params = {}
+    to_remove = []
+    for name, input_ in inputs.items():
+        if not os.path.exists(str(input_)):
+            params[name] = input_
+            to_remove.append(name)
+
+    for name in to_remove:
+        inputs.pop(name)
+
     inputs = {k: sdk.Artifact.load(v) for k, v in inputs.items()
               if v is not None}
 
-    results = action(**inputs)
+    results = action(**inputs, **params)
 
     for name, result in zip(results._fields, results):
         result.save(name)
