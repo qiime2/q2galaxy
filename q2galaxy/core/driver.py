f97538adf2f11e2d84b4141371887a1174657fba
Anthony <oddant1@hotmail.com>
2020-05-18 10:55:57 -0700

    SQUASH: Metadata arguments now work (at least to some degree)

--- q2galaxy/core/driver.py
+++ q2galaxy/core/driver.py
@@ -1,5 +1,7 @@
 import os
 
+import qiime2
+from qiime2.plugin import Metadata, MetadataColumn
 import qiime2.sdk as sdk
 
 
@@ -21,8 +23,21 @@ def action_runner(plugin_id, action_id, inputs):
     for name in to_remove:
         inputs.pop(name)
 
+    metadata_inputs = {}
+    to_remove = []
+    for key, value in inputs.items():
+        type_ = action.signature.parameters[key].qiime_type
+        if type_ == Metadata or type_ == MetadataColumn:
+            metadata_inputs[key] = _convert_metadata(type_, inputs[key])
+            to_remove.append(key)
+
+    for name in to_remove:
+        inputs.pop(name)
+
     inputs = {k: sdk.Artifact.load(v) for k, v in inputs.items()
               if v is not None}
+
+    inputs.update(metadata_inputs)
 
     results = action(**inputs, **params)
 
@@ -34,3 +49,39 @@ def get_version(plugin_id):
     pm = sdk.PluginManager()
     plugin = pm.get_plugin(id=plugin_id)
     return plugin.version
+
+
+def _convert_metadata(input_, value):
+    if input_ == 'MetadataColumn':
+        value, column = value
+    fp = value
+
+    try:
+        artifact = qiime2.Artifact.load(fp)
+    except Exception:
+        try:
+            metadata = qiime2.Metadata.load(fp)
+        except Exception as e:
+            raise ValueError("There was an issue with loading the file %s as "
+                             "metadata:" % fp) from e
+    else:
+        try:
+            metadata = artifact.view(qiime2.Metadata)
+        except Exception as e:
+            raise ValueError("There was an issue with viewing the artifact "
+                             "%s as QIIME 2 Metadata:" % fp) from e
+
+    if input_ != 'MetadataColumn':
+        return metadata
+    else:
+        try:
+            metadata_column = metadata.get_column(column)
+        except Exception:
+            raise ValueError("There was an issue with retrieving column %r "
+                             "from the metadata." % column)
+
+        if metadata_column not in input_:
+            raise ValueError("Metadata column is of type %r, but expected %r."
+                             % (metadata_column, input_.fields[0]))
+
+        return metadata_column
