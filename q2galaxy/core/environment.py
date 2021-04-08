e3cc0c6c718dc223c51c7c0aef68ef1e23388275
Evan Bolyen <ebolyen@gmail.com>
2021-04-08 15:16:56 -0700

    MAINT: update CI for mystery-stew location (#19)

--- q2galaxy/core/environment.py
+++ q2galaxy/core/environment.py
@@ -35,7 +35,11 @@ class CondaMeta:
     def iter_primary_deps(self, package):
         if package not in self.meta_lookup:
             return
-        yield from (dep.split(' ')[0] for dep in self[package]['depends'])
+        yield from (dep.split(' ')[0] for dep in self[package]['depends']
+                    # Ignore conda "virtual packages"
+                    # https://conda.io/projects/conda/en/latest
+                    # /user-guide/tasks/manage-virtual.html
+                    if not dep.startswith('__'))
 
     def iter_deps(self, *packages, include_self=True, _seen=None):
         if _seen is None:
