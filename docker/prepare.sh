dad6fe8ddc24beb50dae91049a36e20fa3bbc5a9
Oddant1 <oddant1@hotmail.com>
2025-04-14 15:35:32 -0700

    Experiment with galaxy flavor

--- docker/prepare.sh
+++ docker/prepare.sh
@@ -5,7 +5,7 @@ cd $(dirname $(realpath -s $0))
 TAG=$(echo $1 | cut -d- -f2 )
 ENV_P=../__tmp_q2galaxy_env_$TAG
 
-conda env create --force -p $ENV_P --file $1
+conda env create -p $ENV_P --file $1
 
 mkdir -p tools/
 rm -rf tools/*
